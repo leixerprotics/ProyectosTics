98c0152c4ae73875d216503807d27967d23df9f6
leixerprotics <Leixer.lucas.tics01@gmail.com>
2025-05-27 18:29:06 -0500

    app

---
+++ appFroms/appFroms/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace appFroms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Location = new System.Drawing.Point(311, 357);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(75, 23);
+            this.btn_ok.TabIndex = 0;
+            this.btn_ok.Text = "OK";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(626, 357);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(75, 23);
+            this.btn_close.TabIndex = 1;
+            this.btn_close.Text = "Close";
+            this.btn_close.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_ok);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.Button btn_close;
+    }
+}
+
---
+++ appFroms/appFroms/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace appFroms
+{
+
+
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string nombre = "Leixer";
+            int edad = 30;
+            uint telefono = 1234567890;
+            float altura = 1.75f;
+            double peso = 70.5;
+            decimal nota = 9.5m;
+
+            bool acceso = true;
+            DateTime fecha = DateTime.Now;
+
+
+            //int numero = 12;
+            //int numero1 = 12;
+            //int numero3= 12;
+            //int numero2 = 12;
+            //int numero4 = 12;
+
+
+            int num7, num9, num8, num10, num11;
+            num7 = 12;num8 = 12; num9 = 12; num10 = 12; num11 = 12;
+
+            const string dato = "Leixer";
+            //dato  = "stteven";
+
+
+
+            MessageBox.Show(fecha.Month.ToString());
+
+            //MessageBox.Show($"{nombre}, edad : {edad}");
+            //MessageBox.Show(peso.ToString());
+            //MessageBox.Show(acceso.ToString());
+             
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+        }
+    }
+}
