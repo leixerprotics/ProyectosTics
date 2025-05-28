042c5b7022290a832e5f4c3d0c9220d2abe6342e
leixerprotics <Leixer.lucas.tics01@gmail.com>
2025-05-28 11:18:31 -0500

    uso de try catch y cadenas

--- appFroms/appFroms/Form1.Designer.cs
+++ appFroms/appFroms/Form1.Designer.cs
@@ -30,36 +30,60 @@
         {
             this.btn_ok = new System.Windows.Forms.Button();
             this.btn_close = new System.Windows.Forms.Button();
+            this.lbl_texto = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btn_ok
             // 
-            this.btn_ok.Location = new System.Drawing.Point(311, 357);
+            this.btn_ok.Location = new System.Drawing.Point(61, 135);
             this.btn_ok.Name = "btn_ok";
-            this.btn_ok.Size = new System.Drawing.Size(75, 23);
+            this.btn_ok.Size = new System.Drawing.Size(108, 38);
             this.btn_ok.TabIndex = 0;
             this.btn_ok.Text = "OK";
             this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
             // 
             // btn_close
             // 
-            this.btn_close.Location = new System.Drawing.Point(626, 357);
+            this.btn_close.Location = new System.Drawing.Point(222, 135);
             this.btn_close.Name = "btn_close";
-            this.btn_close.Size = new System.Drawing.Size(75, 23);
+            this.btn_close.Size = new System.Drawing.Size(108, 38);
             this.btn_close.TabIndex = 1;
             this.btn_close.Text = "Close";
             this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // lbl_texto
+            // 
+            this.lbl_texto.AutoSize = true;
+            this.lbl_texto.Location = new System.Drawing.Point(38, 26);
+            this.lbl_texto.Name = "lbl_texto";
+            this.lbl_texto.Size = new System.Drawing.Size(60, 13);
+            this.lbl_texto.TabIndex = 2;
+            this.lbl_texto.Text = "Bienvenido";
+            this.lbl_texto.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(125, 23);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(146, 20);
+            this.txt_nombre.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(428, 203);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.lbl_texto);
             this.Controls.Add(this.btn_close);
             this.Controls.Add(this.btn_ok);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +91,8 @@
 
         private System.Windows.Forms.Button btn_ok;
         private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Label lbl_texto;
+        private System.Windows.Forms.TextBox txt_nombre;
     }
 }
 
--- appFroms/appFroms/Form1.cs
+++ appFroms/appFroms/Form1.cs
@@ -16,5 +16,41 @@ namespace appFroms
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //USO DE TRY CATCH con valor byte de 255
+                //byte txt_texbox = Convert.ToByte(txt_nombre.Text);
+                ////string txt_texbox = txt_nombre.Text;
+                //lbl_texto.Text = txt_texbox.ToString();
+
+
+                //USO DE CADENAD STRING
+                //string bienvenida = " Bienvenido";
+                //string texto = "hola " + txt_nombre.Text + bienvenida;
+                //lbl_texto.Text = texto;
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
